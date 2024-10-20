327616d913376d4a96933f1125cb867c86a14efb
Sanya Doda <126786063+sanya-2304@users.noreply.github.com>
2024-10-20 15:43:40 +0530

    medium

--- week-2/week-2-js/medium/palindrome.js
+++ week-2/week-2-js/medium/palindrome.js
@@ -4,6 +4,18 @@
 */
 
 function isPalindrome(str) {
+  str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  let s = 0; // Start index
+  let e = str.length - 1; // End index
+  
+  while (s < e) {
+    if (str[s] !== str[e]) {
+      return false; // Not a palindrome
+    }
+    s++; // Move the start index forward
+    e--; // Move the end index backward
+  }
+  
   return true;
 }
 
